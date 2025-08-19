fabbe3e55cc0dedf141db32fbb341b397f09d8ba
faleite <fabricio_2310@hotmail.com>
2025-08-19 23:12:08 +0100

    (Update) First Commit

---
+++ src/test/java/com/faleite/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
